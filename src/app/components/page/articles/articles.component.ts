70e7fc541b4ab9b40ea8ff6c5a4bc1e78c29f06f
Jamie McGrory <contact@jmcgr.co.uk>
2019-01-06 21:57:30 +0000

    updated articles to implement dropdowns

--- src/app/components/page/articles/articles.component.ts
+++ src/app/components/page/articles/articles.component.ts
@@ -3,6 +3,7 @@ import { APIService } from '../../../services/API.service';
 import { CategoryModel } from 'src/app/models';
 import APIFilter from '../../../models/Filter/APIFilter';
 import ArticleModel from '../../../models/ArticleModel';
+import TagModel from '../../../models/TagModel';
 
 @Component({
     selector: 'app-articles',
@@ -11,8 +12,9 @@ import ArticleModel from '../../../models/ArticleModel';
 })
 export class ArticlesComponent implements OnInit {
 
-    categories: CategoryModel[];
-    articles: ArticleModel[];
+    categories: CategoryModel[] = [];
+    tags: TagModel[] = [];
+    articles: ArticleModel[] = [];
 
     constructor(
         private apiService: APIService
@@ -20,6 +22,7 @@ export class ArticlesComponent implements OnInit {
 
     ngOnInit() {
         this.loadArticles();
+        this.loadGroups();
     }
 
     loadArticles = (): void => {
@@ -32,6 +35,38 @@ export class ArticlesComponent implements OnInit {
                 });
             }
         });
+    }
+
+    loadGroups = (): void => {
+        const groupFilter = new APIFilter({});
+        this.apiService.getIds('group', groupFilter).subscribe((ids) => {
+            console.log(ids);
+            this.apiService.getModels('group', ids).subscribe((groups) => {
+                groups.forEach((group) => {
+                    if (group.type === 'tag') {
+                        this.appendTag(group);
+                    } else if (group.type === 'category') {
+                        this.appendCategory(group);
+                    }
+                });
+            });
+        });
+    }
+
+    appendTag = (group: object): void => {
+        this.tags.push(new TagModel(group));
+    }
+
+    appendCategory = (group: object): void => {
+        this.categories.push(new CategoryModel(group));
+    }
+
+    selectTag = (tag: TagModel): void => {
+        console.log(tag);
+    }
+
+    selectCategory = (category: CategoryModel): void => {
+        console.log(category);
     }
 
 }
