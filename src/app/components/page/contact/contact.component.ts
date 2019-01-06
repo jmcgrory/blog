dc62a45aa9eb87d0eaebf1062a57b22ff7fd49a3
Jamie McGrory <contact@jmcgr.co.uk>
2019-01-06 16:12:20 +0000

    basic contact page info

--- src/app/components/page/contact/contact.component.ts
+++ src/app/components/page/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EMAIL_ADDRESS } from '../../../config';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+    public content: string = `If you wish to contact me in regards to any of my past work, had questions regarding an article or were interested in collaborating on any of my on-going packages and projects, feel free to get in contact via <a href="mailto:${EMAIL_ADDRESS}?subject=Website Enquiry">${EMAIL_ADDRESS}</a> and I will be sure to be back to you soon.\n\n If you looking to contact me regarding recruitment possibilities, I am currently happily employed, but thank you for the interest.`;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,11 @@
 const API_URI = 'http://localhost:3000';
 const GIT_URI = 'https://api.github.com';
 const LOC_URI = `http://localhost:4200`;
+const EMAIL_ADDRESS = 'contact@jmcgr.co.uk';
 
 export {
     API_URI,
     GIT_URI,
     LOC_URI,
+    EMAIL_ADDRESS,
 };
